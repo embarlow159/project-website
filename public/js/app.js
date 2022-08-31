618eef14c4aa4218de7b581ef482a957c63cb1d6
Emma Barlow <emma@Emmas-MacBook-Pro.local>
2022-08-31 16:46:26 +0100

    Setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherLocation.addEventListener('submit', (event) => {
     messageOne.textContent = 'Weather loading'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error){
             return messageOne.textContent = data.error
